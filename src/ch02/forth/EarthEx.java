625df221cdd3c3fd86f31003770cbcf208b0314a
JJKKJJKJ <spergals950814@gmail.com>
2025-02-20 17:04:14 +0900

    extends,abstract

--- src/ch02/forth/EarthEx.java
+++ src/ch02/forth/EarthEx.java
@@ -1,5 +1,13 @@
 package ch02.forth;
 
 public class EarthEx {
-
+public static void main(String[] args) {
+		
+//		제일 높은 건물의 높이는???
+		
+		System.out.println("제일 높은 건물" +  Earth.TOP_BUILDING + "입니다");
+		
+		Earth earth = new Earth();
+		System.out.println("제일 높은 건물" + earth.topBuilding + "입니다");
+	}
 }
